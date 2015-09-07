b0b41802c5cf4f9a2e2d968462a6febceaedc15a
cielavenir <cielartisan@gmail.com>
2015-09-07 20:43:36 +0900

    update F#

--- q801/girl.fsx
+++ q801/girl.fsx
@@ -1,3 +1,4 @@
+//usr/bin/env fsharpi $0 $@;exit
 (* locale is broken; cannot submit *)
 open System;;
 Console.WriteLine("ガールフレンドめっちゃほしい！");;
--- tbpgr_colosseum_manager/q791/death1.fsx
+++ tbpgr_colosseum_manager/q791/death1.fsx
@@ -1,2 +1,3 @@
+//usr/bin/env fsharpi $0 $@;exit
 open System;;
 Console.WriteLine("lds".ToUpper());;
